41165fe48cb906cd6b09236e09edb5d47584f60a
BarryS27 <238927804+BarryS27@users.noreply.github.com>
2026-01-01 16:07:09 +0000

    update config.py

--- config.py
+++ config.py
@@ -8,9 +8,29 @@ FILES = {
     'Dream_Majors': 'Dream_Majors.csv'
 }
 
+
+
 COLUMNS = {
     'Grades': ['Sem', 'Level', 'Code', 'Course', 'Weight', 'Q1_Points', 'Q2_Points', 'Q3_Points', 'Q4_Points'],
     'Self_Dev': ['Course', 'From', 'Skill', 'Status', 'Proficiency'],
     'Dream_Schools': ['University', 'School'],
     'Dream_Majors': ['Major']
 }
+
+
+
+TABLE_MAPPING = {
+    'G9': 'Grades',
+    'G10': 'Grades',
+    'G11': 'Grades',
+    'G12': 'Grades',
+    'Self_Dev': 'Self_Dev',
+    'Dream_Schools': 'Dream_Schools',
+    'Dream_Majors': 'Dream_Majors'
+}
+
+
+
+NUMERIC_COLS = [
+    'Weight', 'Q1_Points', 'Q2_Points', 'Q3_Points', 'Q4_Points', 'Proficiency'
+]
